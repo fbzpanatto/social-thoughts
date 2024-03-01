77f3a0a6a070ef2aac00f0167929ef39d425f87d
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-03-01 16:39:00 -0300

    staging

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { ApplicationWrapperComponent } from './components/application-wrapper/application-wrapper.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: ApplicationWrapperComponent },
+  { path: 'home', loadComponent: () => import('./components/application-wrapper/application-wrapper.component').then(m => m.ApplicationWrapperComponent) },
+  { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent) },
   { path: '**', redirectTo: '/home' }
 ];
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ChangeDetectorRef, OnInit } from '@angular/core';
+
+import { BreakpointObserver, MediaMatcher } from "@angular/cdk/layout";
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -18,4 +20,43 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   styleUrls: ['./toolbar.component.css', '../../styles/generic.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ToolbarComponent { }
+export class ToolbarComponent implements OnInit {
+
+  isLargeScreen = false;
+  isMediumScreen = false;
+  isSmallScreen = true;
+  mobileQuery!: MediaQueryList;
+
+  private readonly _mobileQueryListener: (() => void) | undefined;
+
+  constructor(
+    private responsive: BreakpointObserver,
+    media: MediaMatcher,
+    changeDetectorRef: ChangeDetectorRef,
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 960px)')
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener)
+  }
+
+  ngOnInit(): void {
+    this.setBreakpointObserver();
+  }
+
+  setBreakpointObserver() {
+
+    const minWidth720 = '(min-width: 720px)'
+    const minWidth960 = '(min-width: 960px)'
+
+    this.responsive
+      .observe([ minWidth720, minWidth960 ])
+      .subscribe(result => {
+        const breakpoints = result.breakpoints;
+
+        this.isLargeScreen = breakpoints[minWidth960];
+        this.isMediumScreen = !this.isLargeScreen && breakpoints[minWidth720];
+        this.isSmallScreen = this.isLargeScreen && !this.isMediumScreen;
+      });
+  }
+
+}
