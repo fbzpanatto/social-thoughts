6fd9d5c53d9013e39b32771b47e4b6723dda4c23
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-03-02 10:50:08 -0300

    Staging

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, ChangeDetectorRef, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ChangeDetectorRef, OnInit, signal } from '@angular/core';
 
 import { BreakpointObserver, MediaMatcher } from "@angular/cdk/layout";
 
@@ -10,27 +10,19 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule
-  ],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css', '../../styles/generic.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolbarComponent implements OnInit {
 
-
-  isLargeScreen = false;
-  isMediumScreen = false;
-  isSmallScreen = true;
+  minWidth728 = false
+  maxWidth728 = true
+  condition = signal(true)
 
   mobileQuery!: MediaQueryList;
-
   private readonly _mobileQueryListener: (() => void) | undefined;
-
 
   constructor(
     private responsive: BreakpointObserver,
@@ -42,24 +34,22 @@ export class ToolbarComponent implements OnInit {
     this.mobileQuery.addEventListener('change', this._mobileQueryListener)
   }
 
-  ngOnInit(): void {
-    this.setBreakpointObserver();
-  }
+  ngOnInit(): void { this.setBreakpointObserver() }
 
   setBreakpointObserver() {
 
-    const minWidth720 = '(min-width: 720px)'
-    const minWidth960 = '(min-width: 960px)'
+    const minWidth728 = '(min-width: 728px)'
+    const maxWidth728 = '(max-width: 728px)'
 
     this.responsive
-      .observe([ minWidth720, minWidth960 ])
+      .observe([minWidth728, maxWidth728])
       .subscribe(result => {
         const breakpoints = result.breakpoints;
 
-        this.isLargeScreen = breakpoints[minWidth960];
-        this.isMediumScreen = !this.isLargeScreen && breakpoints[minWidth720];
-        this.isSmallScreen = !this.isLargeScreen && this.isMediumScreen;
+        this.minWidth728 = breakpoints[minWidth728];
+        this.maxWidth728 = breakpoints[maxWidth728]
+
+        this.condition.update(curr => curr = !this.minWidth728 && this.maxWidth728)
       });
   }
-
 }
